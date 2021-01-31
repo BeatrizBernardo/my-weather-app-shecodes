4ef9c31dbfda23ae4f265aa3f46c368bd0fb7bd1
Beatriz Bernardo <BeatrizBernardo@users.noreply.github.com>
2021-01-31 19:07:17 +0000

    change images; update forecast by hours

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 //GLOBAL VARIABLES
 var currentUnit = 0; //0 - celsius 1 - fahrenheit
+var currentForecast = 1; //0-days 1- hour
+var currentCity = 0; //0 - currentCity one ; 1 - currentCity off
+var latitude;
+var longitude;
+var unit;
+var newCity = `Lisbon`;
 
 //FUNCTIONS
 function getRealDate() {
@@ -50,6 +56,25 @@ function getRealTime() {
   }
 
   return `${week[weekday]} ${hour}:${minute}`;
+}
+
+function calculateNextHours(d) {
+  let date = new Date(d);
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+
+  if (minute < 10) {
+    minute = `0${minute}`;
+  } else {
+    minute = `${minute}`;
+  }
+  if (hour < 10) {
+    hour = `0${hour}`;
+  } else {
+    hour = `${hour}`;
+  }
+
+  return `${hour}:${minute}`;
 }
 
 function calculateNextDaysDate(d) {
@@ -119,6 +144,8 @@ function getSearchCity(response) {
   let d = document.querySelector("#degrees");
   d.innerHTML = `${degrees}`;
 
+  changeBackgroundImage(degrees);
+
   let clouds = response.data.weather[0].description;
   let imageTemp = document.querySelector("#current-city-data-image");
   imageTemp.innerHTML = `<img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png" style="max-width: 30%;" alt="${clouds}">`;
@@ -141,7 +168,7 @@ function getSearchCity(response) {
 //cities from the list
 function citiesList(event) {
   event.preventDefault();
-  let newCity = event.target.innerHTML;
+  newCity = event.target.innerHTML;
   changeCityElements(newCity);
 }
 
@@ -149,11 +176,12 @@ function citiesList(event) {
 function searchCityByButton(event) {
   event.preventDefault();
   //get the new name
-  let newCity = document.querySelector("#search-field");
-  changeCityElements(newCity.value);
+  newCity = document.querySelector("#search-field").value;
+  changeCityElements(newCity);
 }
 
-function showForecast(response) {
+/*display forecast by days */
+function showForecastDays(response) {
   let forecastElement = document.querySelector(".next-days-data");
   forecastElement.innerHTML = null;
   for (let index = 0; index < 40; index++) {
@@ -175,19 +203,41 @@ function showForecast(response) {
   }
 }
 
+/*Display forecast by hour */
+function showForecastHour(response) {
+  let forecastElement = document.querySelector(".next-days-data");
+  forecastElement.innerHTML = null;
+  for (let index = 0; index < 5; index++) {
+    let date = response.data.list[index].dt * 1000;
+    let temp = Math.round(response.data.list[index].main.temp);
+    let image = response.data.list[index].weather[0].icon;
+    let imageDescription = response.data.list[index].weather[0].description;
+    forecastElement.innerHTML += `
+    <div class="col-sm next-day">
+      <div class="row row-cols-1">
+        <div class="col">${calculateNextHours(date)}</div>
+        <div class="col">${temp}ºC</div>
+        <div class="col">
+          <img src="http://openweathermap.org/img/wn/${image}@2x.png" style="max-width: 35%;" alt="${imageDescription}">
+        </div>
+      </div>
+    </div>`;
+  }
+}
+
 //receive the city to search the weather
 function changeCityElements(newCity) {
-  changeBackgroundImage();
+  currentCity = 1;
   let currentCityName = document.querySelector(".current-city-name");
   currentCityName.innerHTML = `${newCity}`;
 
-  let unit = toStringCurrentUnit(currentUnit);
+  unit = toStringCurrentUnit(currentUnit);
 
   let apiURL = `${apiEndpoint}?appid=${apiKey}&q=${newCity}&units=${unit}`;
   axios.get(apiURL).then(getSearchCity);
 
   let forecastURL = `${apiEndpointForecast}?appid=${apiKey}&q=${newCity}&units=metric`;
-  axios.get(forecastURL).then(showForecast);
+  axios.get(forecastURL).then(showForecastDays);
 }
 
 //current city by button
@@ -195,24 +245,57 @@ function showCurrentCityData(event) {
   event.preventDefault();
 
   navigator.geolocation.getCurrentPosition(function (position) {
-    changeBackgroundImage();
+    currentCity = 0;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-
-    let unit = toStringCurrentUnit(currentUnit);
+    unit = toStringCurrentUnit(currentUnit);
 
     let apiURL = `${apiEndpoint}?appid=${apiKey}&units=${unit}&lat=${latitude}&lon=${longitude}`;
     axios.get(apiURL).then(function (response) {
       let degrees = Math.round(response.data.main.temp);
       let d = document.querySelector("#degrees");
       d.innerHTML = `${degrees}`;
+      changeBackgroundImage(degrees);
+
       let currentCityName = document.querySelector(".current-city-name");
       currentCityName.innerHTML = `${response.data.name}`;
       let forecastURL = `${apiEndpointForecast}?appid=${apiKey}&units=${unit}&lat=${latitude}&lon=${longitude}`;
-      axios.get(forecastURL).then(showForecast);
+      axios.get(forecastURL).then(showForecastDays);
     });
   });
+}
+
+//change forecast by daus or hours
+function changeForecastButton(event) {
+  event.preventDefault();
+  let forecastButton = document.querySelector("#forecastButton");
+
+  if (currentForecast === 0) {
+    forecastButton.innerHTML = `Forecast by Hours`;
+
+    if (currentCity === 0) {
+      let forecastURL = `${apiEndpointForecast}?appid=${apiKey}&units=${unit}&lat=${latitude}&lon=${longitude}`;
+      axios.get(forecastURL).then(showForecastDays);
+    } else {
+      let forecastURL = `${apiEndpointForecast}?appid=${apiKey}&q=${newCity}&units=metric`;
+      axios.get(forecastURL).then(showForecastDays);
+    }
+
+    currentForecast = 1;
+  } else {
+    forecastButton.innerHTML = `Forecast by Days`;
+
+    if (currentCity === 0) {
+      let forecastURL = `${apiEndpointForecast}?appid=${apiKey}&units=${unit}&lat=${latitude}&lon=${longitude}`;
+      axios.get(forecastURL).then(showForecastHour);
+    } else {
+      let forecastURL = `${apiEndpointForecast}?appid=${apiKey}&q=${newCity}&units=metric`;
+      axios.get(forecastURL).then(showForecastHour);
+    }
+
+    currentForecast = 0;
+  }
 }
 
 //function from Math.random MDN
@@ -222,11 +305,22 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-//set background image by random number
-function changeBackgroundImage() {
-  let number = getRandomIntInclusive(1, 20);
-  console.log(number);
-  document.body.style.backgroundImage = `url('images/${number}.jpg')`;
+//set background image by random number by degrees temp
+function changeBackgroundImage(degrees) {
+  let number;
+  let titleElement;
+  //if temperatura is below or equal to 20C change header sentece and image
+  if (degrees <= 20) {
+    number = getRandomIntInclusive(1, 10);
+    document.body.style.backgroundImage = `url('images/${number}.jpg')`;
+    titleElement = document.querySelector(".title");
+    titleElement.innerHTML = `It's cold outside, baby!`;
+  } else {
+    number = getRandomIntInclusive(11, 20);
+    document.body.style.backgroundImage = `url('images/${number}.jpg')`;
+    titleElement = document.querySelector(".title");
+    titleElement.innerHTML = `Let's see how warm is today!`;
+  }
 }
 
 //MAIN
@@ -246,10 +340,13 @@ chooseCity.addEventListener("click", citiesList);
 let currenyCityButton = document.querySelector("#currentCityButton");
 currenyCityButton.addEventListener("click", showCurrentCityData);
 
+let forecastButton = document.querySelector("#forecastButton");
+forecastButton.addEventListener("click", changeForecastButton);
+
 let apiKey = "8e6bcc493a1dde09d842b31c9a0c6dba";
 let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
 let apiEndpointForecast = "https://api.openweathermap.org/data/2.5/forecast";
 
 let imagesURL = "images/";
 
-changeCityElements("Lisbon");
+changeCityElements(newCity);
